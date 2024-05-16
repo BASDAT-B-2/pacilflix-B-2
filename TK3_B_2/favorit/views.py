42c2f13e72a8c90e47a82e5071d62dbc63129f32
adindanurdzykra <adinda.nurdzykra@gmail.com>
2024-05-16 09:33:09 +0700

    membetulkan navigasi

--- TK3_B_2/favorit/views.py
+++ TK3_B_2/favorit/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_favorit(request):
-    return render(request, 'daftar_unduhan.html')
+    return render(request, 'DaftarFavorit.html')
