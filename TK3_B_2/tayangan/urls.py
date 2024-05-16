2891b1dadaa84e6ece44a159460d4d4b3f945feb
arumasekarp <wahyupuri608@gmail.com>
2024-05-17 02:19:57 +0700

    series episode blm fix

--- TK3_B_2/tayangan/urls.py
+++ TK3_B_2/tayangan/urls.py
@@ -6,7 +6,7 @@ app_name = 'tayangan'
 urlpatterns = [
     path('', show_tayangan, name='show_tayangan'),
     path('trailer/', show_trailer, name='show_trailer'),
-    path('series/', show_series, name='show_series'),
+    path('series/<int:series_id>/', show_series, name='show_series'),
     path('film/<uuid:id>', show_film, name='show_film'),
-    path('episode/', show_episode, name='show_episode'),
+    path('series/<int:series_id>/episode/<int:episode_number>/', show_episode, name='show_episode'),
 ]
