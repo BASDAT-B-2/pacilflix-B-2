c9cf8f37dae274a86f89de9dc9704e71e01f3620
Marsya Rahmadani <marsyarahmadani12@gmail.com>
2024-05-16 21:03:02 +0700

    daftar halaman tayangan

--- TK3_B_2/tayangan/views.py
+++ TK3_B_2/tayangan/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import connection
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from datetime import datetime
+from django.http import HttpResponse
+
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def show_series(request):
@@ -10,8 +22,35 @@ def show_episode(request):
 def show_film(request):
     return render(request, "HalamanFilm.html")
 
+#@login_required
 def show_tayangan(request):
-    return render(request, "Tayangan.html")
+    film = []
+    series = []
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f'SELECT * FROM FILM;')
+        film = cursor.fetchall()
+        for i in range(len(film)) :
+            cursor.execute(f'SELECT id, JUDUL, SINOPSIS, url_video_trailer, release_date_trailer FROM TAYANGAN WHERE id = \'{film[i][0]}\'')
+            info = cursor.fetchone()
+            film[i] = info
+        cursor.execute(
+            f'SELECT * FROM SERIES;')
+        series = cursor.fetchall()
+        for i in range(len(series)) :
+            cursor.execute(f'SELECT id, JUDUL, SINOPSIS, url_video_trailer, release_date_trailer FROM TAYANGAN WHERE id = \'{series[i][0]}\'')
+            info_seri = cursor.fetchone()
+            series[i] = info_seri        
+
+    tayangan = film+series
+
+    context = {
+        'tayangan' : tayangan,
+        'series' : series,
+        'film' : film
+    }
+
+    return render(request, "Tayangan.html", context)
 
 def show_trailer(request):
     return render(request, "trailer.html")
