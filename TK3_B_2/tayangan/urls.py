70bb2f1e84ea29df154838561e32c6f20a1a32b7
arumasekarp <wahyupuri608@gmail.com>
2024-05-17 02:41:39 +0700

    episode

--- TK3_B_2/tayangan/urls.py
+++ TK3_B_2/tayangan/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('trailer/', show_trailer, name='show_trailer'),
     path('series/<uuid:series_id>/', show_series, name='show_series'),
     path('film/<uuid:id>', show_film, name='show_film'),
-    path('episode/', show_episode, name='show_episode'),
+    path('episode/<uuid:series_id>/<int:episode_number>/', show_episode, name='show_episode'),
 ]
